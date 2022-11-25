521a920f595406a2aa1812e0db4a4424b76bbf2b
ecknight <ellyknight@gmail.com>
2022-11-25 11:22:04 -0700

    Added a note

---
+++ script/04.ExtractCovariates.R
@@ -0,0 +1,4 @@
+
+
+
+#the coordinates for ABMI Ecosystem health data are buffered within 5.5 km of the actual coordinate and should not be used for covariate extraction. Elly has access to the true coordinates for most of these locations; please ask her to complete that step.
