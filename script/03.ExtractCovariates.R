d8f1d5ed13d0b255ef05000052417baf182fcbd8
ecknight <ellyknight@gmail.com>
2023-03-10 21:49:36 -0700

    Continuing point extraction from GD layers

--- script/03.ExtractCovariates.R
+++ script/03.ExtractCovariates.R
@@ -8,6 +8,8 @@
 
 #This script uses CONUS Albers equal-area conic (EPSG: 5072)
 
+#This script extracts the list of model covariates agreed upon by the BAM National Model team. The full list of those covariates can be found at https://docs.google.com/spreadsheets/d/1XATuq8BOYC2KkbJObaturaf4NFD2ufvn/edit?usp=sharing&ouid=104837701987164094932&rtpof=true&sd=true
+
 #PREAMBLE############################
 
 #1. Load packages----
@@ -49,7 +51,7 @@ loc.buff <- st_buffer(loc.n, 200)
 
 #B. EXTRACT COVARIATES FROM GOOGLE DRIVE####
 
-#1. Climate Normals----
+#1. Climate Normals - static - point value----
 
 #1a. Get list of layers
 cn.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "ClimateNormals"), full.names = TRUE)) %>% 
@@ -62,10 +64,58 @@ cn.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "Clim
 cn <- rast(cn.files$filepath)
 names(cn) <- cn.files$variable
 
-#1c. Extract values
-loc.cn <- exact_extract(cn, loc.buff, 'mean')
+#1c. Extract values - use point value
+loc.cn <- cbind(data.frame(loc.n),
+                loc.n %>% 
+                  st_transform(crs(cn)) %>% 
+                  terra::extract(x=cn, ID=FALSE)) %>% 
+  dplyr::select(-geometry)
 
+#1d. Save output
+write.csv(loc.cn, file.path(root, "CovariateExtraction", "ClimateNormals.csv"), row.names = FALSE)
 
+#2. Topograhic - static - point value----
+
+#2a. Get list of layers
+tp.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "Topography"), full.names = TRUE, pattern="*.tif")) %>% 
+  separate(filepath, into=c("f1", "f2", "f3", "f4", "f5", "f6", "file"), remove=FALSE, sep="/") %>% 
+  mutate(variable = str_sub(file, -100, -5),
+         variable = ifelse(variable=="mTPI_Canada", "mTPI", variable))
+
+#2b. Set up loop
+#extract iteratively because extents and projections do not match
+for(i in 1:nrow(tp.files)){
+  
+  #2c. Read in layer
+  tp <- rast(tp.files$filepath[i])
+  names(tp) <- tp.files$variable[i]
+  
+  #2d. Extract values - use point value
+  if(i==1){
+    loc.tp <- cbind(data.frame(loc.n),
+                    loc.n %>% 
+                      st_transform(crs(tp)) %>% 
+                      terra::extract(x=tp, ID=FALSE)) %>% 
+      dplyr::select(-geometry)
+  } else {
+    loc.tp <- cbind(loc.tp,
+                    loc.n %>% 
+                      st_transform(crs(tp)) %>% 
+                      terra::extract(x=tp, ID=FALSE))
+    
+    loc.tp <- terra::extract(tp, loc.n)
+  }
+  print(paste0("Finished layer ", i, " of ", nrow(tp.files)))
+}
+
+#1d. Save output
+write.csv(loc.tp, file.path(root, "CovariateExtraction", "Topography.csv"), row.names = FALSE)
+
+#3. Peatland depth - static - point value----
+
+#4. Road-----
+
+#5. Human footprint----
 
 
 #C. EXTRACT COVARIATES FROM GEE####
@@ -75,8 +125,8 @@ loc.cn <- exact_extract(cn, loc.buff, 'mean')
 
 #E. STRATIFY####
 
-#F> 
+#F. 
 
-#F. SAVE#####
+#G. SAVE#####
 
 save(visit, bird, "")
