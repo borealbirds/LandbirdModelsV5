7e63ab2dbbe6d6c6e0f25daba53e535d70c65453
ecknight <ellyknight@gmail.com>
2023-03-11 14:38:13 -0700

    Change to loop & lookup table

--- script/03.ExtractCovariates.R
+++ script/03.ExtractCovariates.R
@@ -1,6 +1,6 @@
 # ---
 # title: National Models 4.1 - extract covariates
-# author: Elly Knight, Melina Houle
+# author: Elly Knight
 # created: December 22, 2022
 # ---
 
@@ -10,7 +10,7 @@
 
 #This script extracts the list of model covariates agreed upon by the BAM National Model team. The full list of those covariates can be found at https://docs.google.com/spreadsheets/d/1XATuq8BOYC2KkbJObaturaf4NFD2ufvn/edit?usp=sharing&ouid=104837701987164094932&rtpof=true&sd=true
 
-#This script runs through each category of covariates separately to avoid mistakes. Future versions should consider aligning file names with a lookup table that details extraction method and then looping through all files at once.
+#This script extracts covariates for every combination of location & year. Efficiency could be improved by 15-20% by taking only the unique locations for all rows in the method table with TemporalResolution=="static"
 
 #PREAMBLE############################
 
@@ -22,9 +22,13 @@ library(terra) #basic raster handling
 library(sf) #basic shapefile handling
 library(exactextractr) #fast & efficient raster extraction
 library(rgee) #access & extract layers stored on Google Earth Engine
+library(readxl) #read in lookup google sheet
 
 #2. Set root path for data on google drive----
 root <- "G:/Shared drives/BAM_NationalModels/NationalModels4.1"
+
+#3. Get extraction methods lookup table----
+meth <- read_excel(file.path(root, "NationalModels_V4.1_VariableList.xlsx"), sheet = "ExtractionLookup")
 
 #A. DATA PREP####
 
@@ -39,166 +43,118 @@ loc.yr <- visit %>%
   st_as_sf(coords=c("lon", "lat"), crs=4326, remove=FALSE) %>% 
   st_transform(crs=5072)
 
-#3. Create sf object of just location for static layers----
-loc <- loc.yr %>% 
-  dplyr::select(-year) %>% 
-  unique()
-
 #EXTRA. Test sample dataset----
 set.seed(1234)
-loc.n <- loc %>% 
-  sample_n(1000)
+loc.n <- loc.yr %>% 
+  sample_n(100)
 
-#4. Buffer location objects----
+#3. Buffer location objects----
 loc.buff <- st_buffer(loc.n, 200)
-
-#5. Plain dataframe for joining to output----
-loc.df <- data.frame(loc.n) %>% 
-  dplyr::select(-geometry)
 
 #B. EXTRACT COVARIATES FROM GOOGLE DRIVE####
 
-#1. Climate Normals - static - point value----
+#1. Get list of layers to run----
+meth.gd <- dplyr::filter(meth, Source=="Google Drive", Running==1)
 
-#1a. Get list of layers
-cn.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "ClimateNormals"), full.names = TRUE)) %>% 
-  separate(filepath, into=c("f1", "f2", "f3", "f4", "f5", "f6", "file"), remove=FALSE, sep="/") %>% 
-  separate(file, into=c("n1", "beginning", "end", "variable", "tif"), remove=FALSE) %>% 
-  mutate(variable = ifelse(tif!="tif", paste0(variable, tif), variable)) %>% 
-  dplyr::filter(tif!="18")
+#2. Plain dataframe for joining to output----
+loc.df <- data.frame(loc.n) %>% 
+  dplyr::select(-geometry)
 
-#1b. Read in layers
-cn <- rast(cn.files$filepath)
-names(cn) <- cn.files$variable
-
-#1c. Extract values - use point value
-loc.cn <- cbind(loc.df,
-                loc.n %>% 
-                  st_transform(crs(cn)) %>% 
-                  terra::extract(x=cn, ID=FALSE))
-
-#1d. Save output
-write.csv(loc.cn, file.path(root, "CovariateExtraction", "ClimateNormals_GD.csv"), row.names = FALSE)
-
-#1e. Cleanup
-#rm(cn, cn.files, loc.cn)
-
-#2. Topograhic - static - point or buffer value----
-
-#NOTE: TRI & roughness not working - "cannot read values" - see "Issues" column in tracking sheet for details
-#NOTE: Need to fix column name of exact_extract code
-
-#2a. Get list of layers
-tp.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "Topography"), full.names = TRUE, pattern="*.tif")) %>% 
-  separate(filepath, into=c("f1", "f2", "f3", "f4", "f5", "f6", "file"), remove=FALSE, sep="/") %>% 
-  mutate(variable = str_sub(file, -100, -5),
-         variable = ifelse(variable=="mTPI_Canada", "mTPI", variable),
-         extract = ifelse(variable %in% c("TRI", "roughness"), "radius", "point"))
-
-#2b. Set up loop
-#extract iteratively because extents and projections do not match and mix of extraction
-loc.tp <- loc.df
-for(i in 1:nrow(tp.files)){
+#3. Set up loop----
+for(i in 41:nrow(meth.gd)){
   
-  #2c. Read in layer
-  tp <- rast(tp.files$filepath[i])
-  names(tp) <- tp.files$variable[i]
+  #4. Determine if stacking----
+  #increase i to skip the stacked rows of data
+  if(meth.gd$StackCategory[i]==1){
+    meth.gd.i <- dplyr::filter(meth.gd, Category==meth.gd$Category[i])
+    i <- i + nrow(meth.gd.i) - 1
+  } 
+  if(meth.gd$StackCategory[i]==0){
+    meth.gd.i <- meth.gd[i,]}
   
-  #2d. Extract values - use point value
-  if(tp.files$extract[i]=="point"){
-    loc.tp <- cbind(loc.tp,
-                    loc.n %>% 
-                      st_transform(crs(tp)) %>% 
-                      terra::extract(x=tp, ID=FALSE))
+  #5. Determine if temporally static----
+  if(meth.gd$TemporalResolution[i]=="static"){
+    
+    #6. Read in raster----
+    rast.i <- rast(meth.gd.i$Link)
+    
+    #7. Extract - determine point or buffer extraction---
+    if(meth.gd$Extraction[i]=="point"){
+      loc.cov <- loc.n %>% 
+        st_transform(crs(rast.i)) %>% 
+        terra::extract(x=rast.i, ID=FALSE)
+    }
+    
+    if(meth.gd$Extraction[i]=="radius"){
+      loc.cov <- loc.buff %>% 
+        st_transform(crs(rast.i)) %>% 
+        exact_extract(x=rast.i, "mean")
+    }
+    
   }
   
-  #2e. Extract values - use buffer mean
-  if(tp.files$extract[i]=="radius"){
-    loc.tp <- cbind(loc.tp,
-                    loc.buff %>% 
-                      st_transform(crs(tp)) %>% 
-                      exact_extract(x=tp, "mean", colname_fun="values"))
+  #8. Determine if temporally matching----
+  if(meth.gd$TemporalResolution[i]=="match"){
+    
+    #9. Get list of individual files----
+    files.i <- data.frame(Link=list.files(meth.gd.i$Link, full.names = TRUE),
+                          file=list.files(meth.gd.i$Link)) %>% 
+      mutate(year = as.numeric(str_sub(file, -8, -5))) %>% 
+      arrange(year)
+    
+    #10. Match year of file to year of data----
+    #http://adomingues.github.io/2015/09/24/finding-closest-element-to-a-number-in-a-list/
+    dt = data.table::data.table(year=files.i$year, val = files.i$year)
+    data.table::setattr(dt, "sorted", "year")
+    data.table::setkey(dt, year)
+    loc.n$year.rd <- dt[J(loc.n$year), roll = "nearest"]$val
+    loc.buff$year.rd <- dt[J(loc.buff$year), roll = "nearest"]$val
+    
+    #11. Set up to loop through years----
+    loc.cov <- data.frame()
+    yrs <- unique(files.i$year)
+    for(j in 1:length(yrs)){
+      
+      loc.n.j <- dplyr::filter(loc.n, year.rd==yrs[j])
+      loc.buff.j <- dplyr::filter(loc.buff, year.rd==yrs[j])
+      
+      #12. Read in raster----
+      rast.i <- rast(files.i$Link[j])
+      
+      #13. Extract - determine point or buffer extraction---
+      if(meth.gd$Extraction[i]=="point"){
+        loc.cov <- loc.n.j %>% 
+          st_transform(crs(rast.i)) %>% 
+          terra::extract(x=rast.i, ID=FALSE) %>% 
+          rbind(loc.cov)
+      }
+      
+      if(meth.gd$Extraction[i]=="radius"){
+        loc.cov <- loc.buff.j %>% 
+          st_transform(crs(rast.i)) %>% 
+          exact_extract(x=rast.i, "mean") %>% 
+          data.frame() %>% 
+          rbind(loc.cov)
+      }
+      
+    }
+    
   }
+
+  #14. Add to output----
+  #fix column names
+  if(!is.na(meth.gd$Priority[i])) meth.gd.i$Name <- paste0(meth.gd.i$Name, "-", meth.gd.i$Priority)
+  nms <- c(colnames(loc.df)[1:(ncol(loc.df)-1)], meth.gd.i$Name)
+  loc.df <- cbind(loc.df, loc.cov)
+  names(loc.df) <- nms
   
-  print(paste0("Finished layer ", i, " of ", nrow(tp.files)))
+  #15. Save----
+  save(loc.df, file=file.path(root, "BirdData", "03_NM4.1_data_covariates_GD.R"))
+  
+  #16. Report status----
+  print(paste0("Finished variable ", i, " of ", nrow(meth.gd), " - ", meth.gd$Name[i]))
+
 }
-
-#2f. Save output
-write.csv(loc.tp, file.path(root, "CovariateExtraction", "Topography_GD.csv"), row.names = FALSE)
-
-#2g. Cleanup
-#rm(tp, tp.files, loc.tp)
-
-#3. Wetland - static - buffer value----
-
-#3a. Read in layers
-pt <- rast(file.path(root, "Covariates", "Wetlands", "Peat-ML_global_peatland_extent.tif"))
-names(pt) <- "peat"
-
-#3b. Extract values - use point value
-loc.pt <- cbind(loc.df,
-                loc.buff %>% 
-                  st_transform(crs(pt)) %>% 
-                  exact_extract(x=pt, "mean"))
-
-#3c. Fix column names
-colnames(loc.pt) <- c(colnames(loc.df), names(pt))
-
-#3d. Save output
-write.csv(loc.pt, file.path(root, "CovariateExtraction", "Wetland_GD.csv"), row.names = FALSE)
-
-#3e. Cleanup
-#rm(pt, pt.files, loc.pt)
-
-#4. Disturbance-----
-
-#4a. Get list of layers
-dt.files <- data.frame(filepath = list.files(file.path(root, "Covariates", "Disturbance"), full.names = TRUE, pattern="*.tif")) %>% 
-  separate(filepath, into=c("f1", "f2", "f3", "f4", "f5", "f6", "file"), remove=FALSE, sep="/") %>% 
-  mutate(variable = str_sub(file, -100, -5),
-         variable = case_when(variable=="cum_threat2020.02.18" ~ "CAN_HF_1km",
-                              variable=="Global_HF_CONUS" ~ "Global_HF_1km",
-                              !is.na(variable) ~ variable))
-
-#4b. Set up loop
-#extract iteratively because extents and projections do not match
-loc.dt <- loc.df
-for(i in 1:nrow(dt.files)){
-  
-  #2c. Read in layer
-  dt <- rast(dt.files$filepath[i])
-  names(dt) <- dt.files$variable[i]
-  
-  #2d. Extract values - use point value
-  if(dt.files$extract[i]=="point"){
-    loc.dt <- cbind(loc.dt,
-                    loc.n %>% 
-                      st_transform(crs(dt)) %>% 
-                      terra::extract(x=dt, ID=FALSE))
-  }
-  
-  #2e. Extract values - use buffer mean
-  if(dt.files$extract[i]=="radius"){
-    loc.dt <- cbind(loc.dt,
-                    loc.buff %>% 
-                      st_transform(crs(dt)) %>% 
-                      exact_extract(x=dt, "mean", colname_fun="values"))
-  }
-  
-  print(paste0("Finished layer ", i, " of ", nrow(dt.files)))
-}
-
-#2f. Save oudtut
-write.csv(loc.dt, file.path(root, "CovariateExtraction", "Topography_GD.csv"), row.names = FALSE)
-
-#2g. Cleanup
-#rm(dt, dt.files, loc.dt)
-
-
-#5. Disturbance----
-
-#6. Biomass----
-
 
 #C. EXTRACT COVARIATES FROM GEE####
 
@@ -219,6 +175,9 @@ ee_check()
 poly <- sf_as_ee(loc.buff)
 point <- sf_as_ee(loc.n)
 
+#4. Get list of layers to run----
+meth.gd <- dplyr::filter(meth, Source=="Google Earth Engine", Running==1)
+
 #4. Get layers----
 
 
@@ -230,4 +189,4 @@ point <- sf_as_ee(loc.n)
 
 #G. SAVE#####
 
-save(visit, bird, "")
+save(visit, bird,  file=file.path(root, "01_NM4.1_data_clean.R"))
