e1a3edae18920908b4c5bdc9adbcb3df025078da
MelinaHoule <houle.melina@gmail.com>
2023-11-16 16:16:20 -0500

    Update scripts

--- scriptMH/Predictive_rasters/ETHheight.R
+++ scriptMH/Predictive_rasters/ETHheight.R
@@ -79,69 +79,9 @@ ETHheightcv1k <- terra::crop(r_ETHheightcv1k, rast1k, extend = TRUE) # Extend to
 r_ETHheightcv5k<-terra::focal(r_ETHheightcv1k,w=matrix(1,5,5),fun=mean,na.rm=TRUE) #get 5k average at 1k resolution
 ETHheightcv5k <- terra::crop(r_ETHheightcv5k, rast1k, extend = TRUE) # Extend to NatMod extent
 
-#cv 5x5
-
 #write output
 writeRaster(ETHheight, filename="./ETHheight.tif", filetype="GTiff", overwrite=TRUE)
 writeRaster(ETH5k, filename="./ETHheight5k.tif", filetype="GTiff", overwrite=TRUE)
 writeRaster(ETHheightcv1k, filename="./ETHheightcv.tif", filetype="GTiff", overwrite=TRUE)
 writeRaster(ETHheightcv5k, filename="./ETHheightcv5k.tif", filetype="GTiff", overwrite=TRUE)
-
-
-
-
-
-
-# Convert to SPatRast (terra format)
-spat_rast <- rast(temp)
-# Reproject
-spat_rast.pj <- project(spat_rast, rast1km, res = 1000, method = "bilinear", align = TRUE)
-# Crop to NatMod extent
-rast.cp <- crop(spat_rast.pj, ext(rast1km), extend = TRUE)
-  
-#write output
-writeRaster(rast.cp, filename=file.path(out_f,paste0("RAP_TCC1km_",as.character(yrlist[i]), ".tif")), filetype="GTiff", overwrite=TRUE)
-file.remove(rastlist)
-
-
-#get 5k average at 1k resolution
-rastlist <- list.files(path = out_f, pattern="RAP_TCC1km_", 
-                       all.files=TRUE, full.names=FALSE)
-rasters_1km <-lapply(file.path(out_f, rastlist), rast)
-rasters5k<-lapply(rasters_1km, function(x) {focal(x,w=matrix(1,5,5),fun=mean,na.rm=TRUE)})
-for (i in 1:length(rastlist)) {names(rasters5k[[i]]) <- paste0("RAP_TCC5x5_", substr(rastlist[i],12,15), ".tif")}
-lapply(rasters5k, function(i) {
-  writeRaster(i, filename=file.path(out_f,names(i)), filetype="GTiff", overwrite = TRUE)
-})
-
-
-
-
-  img_1k <- ee_as_raster(
-     img,
-      via = "drive",
-      dsn = "ORNLbiomass.tif",
-      #scale = 1000,
-      container = "rgee_backup",
-      maxPixels = 4e+09,
-      lazy = FALSE,
-      public = TRUE,
-      add_metadata = TRUE,
-      timePrefix = TRUE,
-      quiet = FALSE,
-   )
-  return(img_1k)
-}
-
-# Run function to extract
-carbonDen.extract <- carbonDen(yr)
-carbonDen1k <- rast(carbonDen.extract)
-carbonDen_pj <- project(carbonDen1k, rast1k, res = 1000, method = "near", align = TRUE)
-carbonDen_cp <-crop(carbonDen_pj, ext(rast1k), extend = TRUE)
-writeRaster(carbonDen_cp, filename=file.path(out_f, "ORNLbiomass1km.tif"), overwrite=TRUE)
-
-writeRaster(carbonDen_pj, filename=file.path(out_f, "ORNLbiomass1km_notcrop.tif"), overwrite=TRUE)
-writeRaster(carbonDen1k, filename=file.path(out_f, "ORNLbiomass1km_rast.tif"), overwrite=TRUE)
-
-
 
