b3934069a76ed10f66295fee5119f5fe320f7262
ecknight <ellyknight@gmail.com>
2024-01-02 13:15:59 -0700

    Initial commit

---
+++ analysis/06.Tune/06b.Tune_PickLR.R
@@ -0,0 +1,18 @@
+# ---
+# title: National Models 5.0 - tune: pick learning rate
+# author: Elly Knight
+# created: January 2, 2024
+# ---
+
+#NOTES################################
+
+#PREAMBLE############################
+
+#1. Load packages----
+library(tidyverse) #basic data wrangling
+
+#2. Set root path for data on google drive----
+root <- "G:/Shared drives/BAM_NationalModels/NationalModels5.0"
+
+#3. Turn off scientific notation---
+options(scipen=99999)
