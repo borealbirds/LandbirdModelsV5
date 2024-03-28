ed03fd29bf00b538178c52fcff7e23a12710950d
ecknight <ellyknight@gmail.com>
2024-03-28 15:15:23 -0600

    building function

--- analysis/08.Predict/08.Predict.R
+++ analysis/08.Predict/08.Predict.R
@@ -18,14 +18,11 @@ library(terra)
 
 #2. Determine if testing and on local or cluster----
 test <- TRUE
-cc <- TRUE
+cc <- FALSE
 
 #3. Set nodes for local vs cluster----
 if(cc){ nodes <- 32}
 if(!cc){ nodes <- 2}
-
-#3. Set species subset if desired----
-sppuse <- c("OVEN", "OSFL", "CONW", "PAWA")
 
 #4. Set root path for data on google drive (for local testing)----
 root <- "G:/Shared drives/BAM_NationalModels/NationalModels5.0"
@@ -52,12 +49,14 @@ tmpcl <- clusterEvalQ(cl, library(tidyverse))
 tmpcl <- clusterEvalQ(cl, library(Matrix))
 tmpcl <- clusterEvalQ(cl, library(terra))
 
-#8. Load the stacks----
+#8. Load the region file----
+bcr <- read_sf(file.path("data", "BAM_BCR_NationalModel_Buffered.shp"))
 
+#11. Load data objects----
+print("* Loading data on workers *")
 
-#tpmcl <- clusterEvalQ()
-
-
+if(cc){ tmpcl <- clusterEvalQ(cl, setwd("/home/ecknight/NationalModels")) }
+tmpcl <- clusterExport(cl, c("bcr"))
 
 #WRITE FUNCTION##########
 
@@ -69,14 +68,28 @@ brt_predict <- function(i){
   bcr.i <- loop$bcr[i]
   spp.i <- loop$spp[i]
   boot.i <- loop$boot[i]
+  year.i <- loop$year[i]
   
   #2. Load model----
+  load(paste0("output/bootstraps/", spp.i, "_", bcr.i, "_", boot.i, ".R"))
   
   #3. Load raster stack----
+  stack.i <- rast(paste0("stacks/", bcr.i, "_", year.i, ".tif"))
+  names(stack.i) <- c("meth.i", names(stack.i)[2:dim(stack.i)[3]])
   
   #4. Predict----
+  pred.i <- terra::predict(model=b.i, object=stack.i)
   
+  #5. Get the region polygon----
+  shp.i <- dplyr::filter(bcr, country==str_sub(bcr.i, 1, 3), subUnit==as.numeric(str_sub(bcr.i, 4, 10))) %>% 
+    vect()
+  
+  #6. Mask----
+  mask.i <- mask(pred.i, shp.i)
+
   #5. Save----
+  
+  
   
   
   
@@ -92,22 +105,22 @@ brt_predict <- function(i){
 years <- 2020
 
 #2. Get list of models that are tuned----
-tuned <- data.frame(path = list.files("output/bootstraps", pattern="*.csv", full.names=TRUE),
-                    file = list.files("output/bootstraps", pattern="*.csv")) %>% 
-  separate(file, into=c("step", "spp", "bcr", "lr", "boot"), sep="_", remove=FALSE)
+booted <- data.frame(path = list.files("output/bootstraps", pattern="*.R", full.names=TRUE),
+                    file = list.files("output/bootstraps", pattern="*.R")) %>% 
+  separate(file, into=c("spp", "bcr", "boot"), sep="_", remove=FALSE) %>% 
+  mutate(boot = as.numeric(str_sub(boot, -100, -3)))
 
 #5. Create to do list----
 #Sort longest to shortest duration to get the big models going first
-todo <- perf %>% 
-  dplyr::select(bcr, spp, lr, trees, time) %>% 
-  expand_grid(boot=c(1:boots)) %>% 
-  arrange(-time)
+todo <- booted %>% 
+  dplyr::select(bcr, spp, boot) %>% 
+  expand_grid(year=years)
 
 #6. Determine which are already done----
-done <- data.frame(path = list.files("output/bootstraps", pattern="*.csv", full.names=TRUE),
-                   file = list.files("output/bootstraps", pattern="*.csv")) %>% 
-  separate(file, into=c("step", "spp", "bcr", "boot"), sep="_", remove=FALSE) %>% 
-  mutate(boot = as.numeric(str_sub(boot, -100, -5)))
+done <- data.frame(path = list.files("output/predictions", pattern="*.csv", full.names=TRUE),
+                   file = list.files("output/predictions", pattern="*.csv")) %>% 
+  separate(file, into=c("spp", "bcr", "boot"), sep="_", remove=FALSE) %>% 
+  mutate(boot = as.numeric(str_sub(boot, -100, -3)))
 
 #7. Create final to do list----
 if(nrow(done) > 0){
