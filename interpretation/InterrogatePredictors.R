ca2b71ba8cd2c1e18f51b3f4a6d04fb38fdd8d89
mannfred boehm <mannfred.boehm@ubc.ca>
2024-05-27 16:32:22 -0700

    creating index for looping function

--- interpretation/InterrogatePredictors.R
+++ interpretation/InterrogatePredictors.R
@@ -16,30 +16,75 @@
 #PREAMBLE############################
 
 #1. Attach packages----
-print("* Attaching packages on master *")
 library(tidyverse)
 
 
 
 #WRITE FUNCTION#######################
+# (this will eventually be a function for use in a package/shinyapp)
 
+
+
+#1. Connect to BAM Drive and create an index of output attributes----
+root <- "G:/Shared drives/BAM_NationalModels/NationalModels5.0/output/bootstraps"
+
+prediction_files <- list.files(root)
+
+loop <- 
+  prediction_files %>% 
+  stringr::str_split_fixed(pattern="_", n=3) %>% 
+  dplyr::as_tibble() %>% 
+  magrittr::set_colnames(c("spp", "bcr", "boot")) %>% 
+  dplyr::arrange(spp, bcr)
 
 
 
 #2. Generate covariate lists (remove covs that explain < 0.01 % of deviance)----
-# (Based on code from "06.Bootstrap.R")
+# Based on code from "06.Bootstrap.R")
 
-covsnew <- list()
+covs <- list()
 for(i in 1:nrow(loop)){
   
-  load(file=file.path(root, "output", "fullmodels", paste0(loop$spp[i], "_", loop$bcr[i], "_", loop$lr[i], ".R")))
+  # define spp x bcr x bootstrap permutation
+  bcr.i <- loop$bcr[i]
+  spp.i <- loop$spp[i]
+  boot.i <- loop$boot[i]
+  # lr.i <- loop$lr[i]
+  # id.i <- 3
+  
+  # loads a `gbm` object named `m.i`. 
+  # `m.i$contributions` is a `data.frame` with columns `var` and `rel.inf`
+  load(file=file.path(root, "output", "bootstraps", paste0(loop$spp[i], "_", loop$bcr[i], "_", loop$lr[i], ".R")))
   
   #make sure to retain method and year
-  covsnew[[i]] <- m.i[["contributions"]] %>% 
+  covs[[i]] <- m.i[["contributions"]] %>% 
     dplyr::filter(rel.inf >= 0.1 |
                     var %in% c("year", "method"))
   
 }
 
 print("* Loading new covariate lists *")
-tmpcl <- clusterExport(cl, c("covsnew"))
+tmpcl <- clusterExport(cl, c("covs"))
+
+
+
+
+
+# birdlist <- data.frame(bcr=bcrs)
+
+# # create `loop` object (see: "05.Tune.R")
+# bcr.spp <- birdlist %>% 
+#   pivot_longer(AGOS:YTVI, names_to="spp", values_to="use") %>% 
+#   dplyr::filter(use==TRUE) %>% 
+#   dplyr::filter(spp %in% sppuse) %>% 
+#   dplyr::select(-use)
+# 
+# 
+# loop <- bcr.spp %>% 
+#   anti_join(done) %>% 
+#   anti_join(redo) %>% 
+#   mutate(lr = 0.001) %>% 
+#   rbind(redo %>% 
+#           dplyr::select(spp, bcr, lr.next) %>% 
+#           rename(lr = lr.next)) %>% 
+#   arrange(spp, bcr)
