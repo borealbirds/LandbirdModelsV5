f0bdb2286c15550e400323e79b35e750b40b47ba
ecknight <ellyknight@gmail.com>
2024-05-28 20:24:35 -0600

    merge ca and us code, change shapefile source

--- gis/WeightingRasters.R
+++ gis/WeightingRasters.R
@@ -19,13 +19,13 @@ library(tidyverse)
 library(terra)
 
 #1. NAD83(NSRS2007)/Conus Albers projection (epsg:5072) ----
-crs<-"+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
+crs <- "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
 
 #2. Set file paths ----
-root<-"G:/Shared drives/BAM_NationalModels/NationalModels5.0" #PC link
+root <- "G:/Shared drives/BAM_NationalModels/NationalModels5.0" #PC link
 setwd(file.path(root,"MosaicWeighting"))
 
-#3.Get Canada and USA BCR boundaries ----
+#3. Get Canada and USA BCR boundaries ----
 
 usa <- read_sf(file.path(root, "Regions", "USA_adm", "USA_adm0.shp")) %>% 
   st_transform(crs=crs) # US boundary
@@ -45,13 +45,17 @@ bcr.ca <- read_sf(file.path(root, "Regions", "BAM_BCR_NationalModel.shp")) %>%
   st_intersection(can) %>% 
   mutate(country="ca") # Restrict to Canadian BCRs
 
+#Buffered shapefile
+bcr <- read_sf(file.path(root, "Regions", "BAM_BCR_NationalModel_Buffered.shp")) |> 
+  mutate(bcr = paste0(country, subUnit))
+
 #4. Determine overlap zones for Canada and US  -----
 
 Baselayer<-terra::rast(file.path(root, "PredictionRasters/ClimateNormal/AHM_1km.tif")) #any prediction raster
 Baselayer[!is.na(Baselayer)] <- 1
 e<-ext(Baselayer)
 
-OverlapCan<-c()
+OverlapCan <- c()
 
 for (i in (1:nrow(bcr.ca))) {
   bcr.i <-  bcr.ca %>% 
@@ -105,37 +109,38 @@ MosaicOverlap2<-terra::rast(file.path(root,"MosaicWeighting","ModelOverlap_US.ti
 MosaicOverlap <- mosaic(MosaicOverlap1, MosaicOverlap2)
 writeRaster(MosaicOverlap, file.path(root, "MosaicWeighting","ModelOverlap.tif", sep=""), overwrite=TRUE)
 
-#5.Produce distance-to-the-edge raster for every BCR (for model weighting) ---------------
-# ***Note*** this extracts Canada, change selection where noted to produce US BCR outputs
+#5. Produce distance-to-the-edge raster for every BCR (for model weighting)---------------
 
-for (i in (1:nrow(bcr.ca))) {  #***for US BCRs: bcr.ca==bcr.us/can==usa
-  bcr.i <-  bcr.ca %>% 
-    dplyr::filter(row_number()==i) %>% 
-    st_buffer(100000)%>%  # Filter & buffer shapefile
-    st_intersection(., can) #crop at CAN border
+for (i in 32:nrow(bcr)){
+  
+  bcr.i <-  bcr[i,]
   
 # Make fishnet (10km2 to speed processing - could make this finer)
-  grid <- st_make_grid(bcr.i, cellsize = 10000, what = "centers")
-  grid <- st_intersection(grid, bcr.i)  
+  grid <- st_make_grid(bcr.i, cellsize = 10000, what = "centers") |> 
+    st_intersection(bcr.i)
   
 # Polygon -> line
-  bcr.i <- st_cast(bcr.i, "MULTILINESTRING")
+  bcr.line <- st_cast(bcr.i, "MULTILINESTRING")
   
 # Get distance from the edge for each point
-  dist <- st_distance(bcr.i, grid)
+  dist <- st_distance(bcr.line, grid)
   
 # Dataframe as proportion of max distance
   df <- data.frame(dist = as.vector(dist)/as.vector(max(dist)),
                    st_coordinates(grid))
   
 # Rasterize + match resolution
-  ext <- ext(bcr.i)
-  r <- terra::rast(resolution = 10000, ext = ext, crs = crs)
-  dist_sf <- st_as_sf(df, coords = c("X", "Y")) %>% st_set_crs(crs)
-  dist_raster <- terra::rasterize(dist_sf, r, "dist", fun = mean) %>% terra::disagg(.,fact=10, method="bilinear")
-  writeRaster(dist_raster,paste(root,"MosaicWeighting/CAN_BCR_Weighting/","EdgeWeighting_BCR_", bcr.i$subUnit, ".tif", sep=""))
+  r <- terra::rast(resolution = 10000, ext = ext(bcr.line), crs = crs)
+  
+  dist_sf <- st_as_sf(df, coords = c("X", "Y")) |> 
+    st_set_crs(crs)
+  
+  dist_raster <- rasterize(dist_sf, r, "dist", fun = mean) |> 
+    terra::disagg(fact=10, method="bilinear")
+  
+  writeRaster(dist_raster, paste0(root, "/MosaicWeighting/BCR_Weighting/","EdgeWeighting_", bcr.i$bcr, ".tif"))
+  
+  cat("Finished", i, "of", nrow(bcr), "BCRs \n")
 }  
-
-#*** For US BCRs change save to: writeRaster(dist_raster,paste(root,"MosaicWeighting/USA_BCR_Weighting/","EdgeWeighting_USA_BCR_", bcr.i$subUnit, ".tif", sep=""))
 
 ###################### END OF CODE #####################################
