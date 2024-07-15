6d5a1bf59286709de438b8475ed1d99ae5b198d0
ecknight <ellyknight@gmail.com>
2024-07-15 13:49:59 -0600

    Some notes

---
+++ analysis/12.ValidateMosaics.R
@@ -0,0 +1,11 @@
+# ---
+# title: National Models 5.0 - validate subunit predictions
+# author: Elly Knight
+# created: April 29, 2024
+# ---
+
+#NOTES################################
+
+# This script uses the withheld data from each bootstrap to validate the spatial the models.
+
+# Validation is done using predictions for each subunit model from the `11.ValidateSubunits.R` script.
