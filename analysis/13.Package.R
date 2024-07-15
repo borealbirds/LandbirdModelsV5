4678931df25fca2aa1c6d930197fb74dc6cdc0f8
ecknight <ellyknight@gmail.com>
2024-07-15 13:05:12 -0600

    Update to do list

--- analysis/13.Package.R
+++ analysis/13.Package.R
@@ -1,9 +1,10 @@
+#TO DO: PACKAGE VALIDATION: MEAN & SD OF VALIDATIONS######
 #TO DO: ADD SAMPLING PER SPECIES#######
+#TO DO: FINAL PREDICTION RASTER STACKS
 
 #LIST OF PRODUCTS PER SPECIES:
 #2 extents: boreal, regional
 #Products in both: mean, sd, extrapolation, sampling
-
 
 #LIST OF SAMPLING PRODUCT:
 #All the extrapolation
