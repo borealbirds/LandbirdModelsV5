c569d054eaab4a96bc281ff8b3d4e2152fd95e85
ecknight <ellyknight@gmail.com>
2024-07-22 13:19:20 -0600

    Add transformation to do

--- analysis/13.Package.R
+++ analysis/13.Package.R
@@ -1,6 +1,15 @@
 #TO DO: PACKAGE VALIDATION: MEAN & SD OF VALIDATIONS######
-#TO DO: ADD SAMPLING PER SPECIES#######
+
+#17. Package and save----
+out <- data.table::rbindlist(out.list, fill=TRUE)
+
+write.csv(out, file.path(root, "output", "validation", "ModelValidation_BCR.csv"), row.names = FALSE)
+
+
+#TO DO: SPECIES GUILDS STACKS#######
+
 #TO DO: FINAL PREDICTION RASTER STACKS
+#REPROJECT TO LAEA HERE
 
 #LIST OF PRODUCTS PER SPECIES:
 #2 extents: boreal, regional
