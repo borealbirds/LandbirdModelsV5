05c198f7ca15fdb9d65811642b1e059428222007
Anna Drake <52977092+A-Drake@users.noreply.github.com>
2024-09-18 15:37:27 -0600

    Update GeographicVisualization.R
    Update regression function

--- interpretation/GeographicVisualization.R
+++ interpretation/GeographicVisualization.R
@@ -24,13 +24,17 @@ root <- "G:/Shared drives/BAM_NationalModels5"
 
 # Custom pixel-level trend function -----
 reg <-  function(val) { 
-  if(length(subset(val,val>=0))>2) { # at least 3 predictions in stack
-    reg<-lm(x~y, data = data.frame(x = val, y = 1:nlyr(val)))
-    if(base::summary(reg)$coefficients[2,4]<=0.05) { # and slope is significant
-      base::summary(reg)$coefficients[2,1] # return slope
-    }}
-  else {NA} # otherwise NA
+  if(sum(val)==0){NA} # return NA where no birds
+  else{
+  if (length(subset(val,val>=0))>2) { # if at least 3 predictions in stack
+      start<-mean(val[1:10]) #calculate 1985 mean as baseline
+      val<-val/start #set value as proportion of baseline
+      rg<-lm(x~y,data = data.frame(x = val, y=rep(1:8, each=10))) #run lm (raster stacking is 10 boots x year)
+  if (base::summary(rg)$coefficients[2,4]<=0.05) { # and if slope is significant
+      base::summary(rg)$coefficients[2,1] # return slope
 }
+  else{NA} # otherwise return NA where slope is NS
+  }
 
 # Colour formatting --- 
 colfunc<-colorRampPalette(c("lightyellow","darkgreen","navy")) # range colours
@@ -65,7 +69,7 @@ list<-dir(root1)
 spp<-unique(substr(list,1,4))
 yr<-c(1985,1990,1995,2000,2005,2010,2015,2020)
 
-for(j in c(5:length(spp))) {
+for(j in c(1:length(spp))) {
 
 #1. Import species range limit - using 0.2% occurrence ---
 range<-st_read(paste0("G:/Shared drives/BAM_NationalModels5/MosaicWeighting/Range/ShapeFile/",spp[j],"_rangelimit.shp"))
@@ -119,11 +123,11 @@ out<-sum(layer1,mean[[i]], na.rm=T)
 layer2<-mask(layer,range[2], inverse=T) 
 out2<-mask(out,range[2])
 
-#join to background and reproject ----
+#join to background and reproject to Canada Lambert ----
 final<-sum(layer2,out2, na.rm=T)
 final<-terra::project(final,crs("EPSG:3348"))
 
-#5. save out image ----
+#6. save out image ----
 png(paste("Mean_",spp[j],"_",yr[i],".png", sep=""),width=12.2,
     height=9,units = "in",res = 1200)
 
@@ -135,22 +139,49 @@ plot(plotlimit$geometry,lwd=0.5, border="darkred",add=T)
 plot(bcr$geometry,lwd=0.6, border="grey50",add=T)
 dev.off()
   }
-#}
 
-#6. Calculate pixel-level trends ----
+#7. Calculate pixel-level trends ----
 
-slope <- app(trend, fun=reg)
+slope <- app(trend, fun=reg)  # note this has a 5-year interval, so result is proportion change/5 yrs
+
+# reproject to Canada Lambert ----
+slope_map<-terra::project(slope_sm,crs("EPSG:3348"))  
 
 # Center range on 0 for visualization -----
-range<-global(slope, quantile, probs=c(0.01,0.99), na.rm=TRUE)%>%unlist()%>%as.vector()
-lower<-seq(range[1],(-0.0005),0.0005)
-upper<-seq(0.0005,range[2],0.0005)
-breakd<-c(-0.5,lower,0,upper,0.5)
-diverge<-c(Low(length(lower)+1),"white",High(length(upper)+1))
+range<-global(slope_map, quantile, probs=c(0.01,0.99), na.rm=TRUE)%>%unlist()%>%as.vector()
+step<-round((range[2]-range[1])/10,4) # assign interval of 10 breaks
 
-# Plot output ------
-plot(bcr$geometry, col="grey")
-plot(sigslope,col=diverge,breaks=breakd, add=T)
+lower<-round(seq(as.integer(range[1]/step)*step,(-(step)),step),4)
+upper<-round(seq(step,as.integer(range[2]/step)*step,step),4)
 
+diverge<-c(Low(length(lower)),"white",High(length(upper)))
+brk<-c(lower,0,upper)
+lim<-c((min(brk))-(step*0.5),(max(brk))+(step*0.5))
+plg <-list(loc = "bottom",at = brk, cex=2)
 
+# R plot drops out-of-range values so need to set to max/min -------
+slope_map[slope_map<lim[1]]<-lim[1]
+slope_map[slope_map>lim[2]]<-lim[2]
+
+# 8. Plot trend output ------
+  png(paste0("1985-2020_PredictedTrend_1k_",spp[j],".png"),width=15,
+      height=10, units = "in",res = 1200)
+  plot(bcr$geometry, col="grey80", main=paste0("Trend/ 5yrs ", spp[j]))
+  plot(ocean$geometry, lwd=0.1,col="black", border = "black", add=T)
+  plot(slope_map,col=diverge, range=lim, plg=plg, add=T)
+  plot(water$geometry,lwd=0.05,col="black",border = "black", add=T)
+  plot(plotlimit$geometry,lwd=0.7, border="darkgreen",add=T) 
+  plot(bcr$geometry,lwd=0.7, border="black", add=T)
+  dev.off()
+  #} #close spp loop
 ##################### End of Code ###################
+
+  #testing lower resolution - need to think on the correct approach
+  #should we run regression on 9 values per layer (possibly pseudo-replication?) or average prior?
+  # Produce a reference layer ----
+  r2 <- trend[[1]]
+  res(r2) <- 3000 #set to 3km2
+  # Average boots over 9 pixels ----
+  trend_sm<-resample(trend,method="average",r2) 
+  # run regression on lower resolution result ----
+  slope_sm<-app(trend_sm, fun=reg)
