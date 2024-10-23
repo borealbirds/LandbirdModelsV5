22454c0d509bd43e76845bad85723c59900773a3
ecknight <ellyknight@gmail.com>
2024-10-23 16:17:21 -0600

    more notes

--- analysis/12.Package.R
+++ analysis/12.Package.R
@@ -1,19 +1,22 @@
-#TO DO: PACKAGE VALIDATION: MEAN & SD OF VALIDATIONS######
+
 
 #17. Package and save----
 out <- data.table::rbindlist(out.list, fill=TRUE)
 
 write.csv(out, file.path(root, "output", "validation", "ModelValidation_BCR.csv"), row.names = FALSE)
 
-
-#TO DO: SPECIES GUILDS STACKS#######
-
 #TO DO: FINAL PREDICTION RASTER STACKS
 #REPROJECT TO LAEA HERE
 
+#TO DO: AVERAGE THE VALIDATION METRICS####
+
+#TO DO: ADD ATTRIBUTES FOR SP ETC
+
+#TO DO: CHECK EVERYTHING IS THERE#######
+
 #LIST OF PRODUCTS PER SPECIES:
 #2 extents: boreal, regional
-#Products in both: mean, sd, extrapolation, sampling
+#Products in both: mean, cv, extrapolation, sampling
 
 #LIST OF SAMPLING PRODUCT:
 #All the extrapolation
