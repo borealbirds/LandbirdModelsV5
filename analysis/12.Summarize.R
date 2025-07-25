e73fd32c0670d5ff708c5bca163c83023fd871e1
ecknight <ellyknight@gmail.com>
2025-07-25 10:41:05 -0600

    Initial structure

--- analysis/12.Summarize.R
+++ analysis/12.Summarize.R
@@ -6,13 +6,16 @@
 
 #NOTES################################
 
-# This script summarizes the output from the `10.Validate.R` script into a single dataframe to be included with model products
+# This script summarizes the output from the `10.Validate.R` alongside additional model metadata.
+
+# The output is a .xlsx file similar to the V4 "results" object.
 
 #PREAMBLE############################
 
 #1. Load packages----
 library(tidyverse) #data wrangling
 library(data.table) #rbinding lists to dataframe with filling unmatched columns
+library(readxl)
 
 #2. Set root path----
 root <- "G:/Shared drives/BAM_NationalModels5"
@@ -20,27 +23,52 @@ root <- "G:/Shared drives/BAM_NationalModels5"
 #3. Restrict scientific notation----
 options(scipen=999999)
 
-#INVENTORY#############################
+#SPECIES###########
 
-#1. List of validated models-----
-validated <- data.frame(file = list.files(file.path(root, "output", "validation")),
-                        path = list.files(file.path(root, "output", "validation"), full.names = TRUE)) |> 
-  separate(file, into=c("species", "boot", "filetype"))
+#1. Get V4 object -----
+species.4 <- read_excel(file.path(root, "data", "Lookups", "BAMv4-results-2025-05-21.xlsx"), sheet="species")
 
-#2. Get todo list----
-loop <- validated |> 
-  group_by(species) |> 
-  summarize(boots = n()) |> 
-  ungroup() |> 
-  dplyr::filter(boots==10)
+#2. Get list of packaged species----
+packaged <- data.frame(file = list.files(file.path(root, "output", "10_packaged"), pattern="*.tif", recursive=TRUE))  |> 
+  separate(file, into=c("sppfolder", "bcrfolder", "spp", "bcr", "year", "filetype"), remove=FALSE) |> 
+  mutate(year = as.numeric(year)) |> 
+  dplyr::filter(sppfolder!="extrapolation",
+                bcr=="mosaic",
+                year==2020) |> 
+  dplyr::select(spp)
 
-#SUMMARIZE###########
+#3. Get list of validatd species----
+validated <- data.frame(file = list.files(file.path(root, "output", "11_validated"), pattern=".Rdata")) |> 
+  separate(file, into=c("spp", "filetype")) |> 
+  dplyr::select(spp)
+
+#4. Get list of species ready to summarize----
+spp <- inner_join(packaged, validated)
+
+#5. Join to species info from v4 ----
+species <- species.4 |> 
+  inner_join(spp |> 
+               rename(id = spp))
+
+#REGIONS############
+
+
+#VARIABLES########
+
+#1. Get it from google drive----
+variables <- read.csv(file.path(root, "data", "Lookups", "covariate_metadata.csv"))
+
+#IMPORTANCE#########
+
+#VALIDATION#############################
+
+#TO DO:UPDATE FOR THE NEW FORMAT#######
 
 #1. Set up loop----
 eval.out <- list()
-for(i in 1:nrow(todo)){
+for(i in 1:nrow(spp)){
   
-  species.i <- loop$species[i]
+  species.i <- spp$spp[i]
   
   #2. List of Rdata files----
   loop.i <- dplyr::filter(validated, species==species.i)
@@ -70,7 +98,18 @@ for(i in 1:nrow(todo)){
 }
 
 #5. Final object----
-eval <- rbindlist(eval.out, fill=TRUE)
+validation <- rbindlist(eval.out, fill=TRUE)
 
-#6. Save ----
-write.csv(eval, file.path(root, "output", "packaged", "BAMV5ModelEvaluationSummary.csv"), row.names = FALSE)
+#ABUNDANCES##########
+
+
+
+#DENSITIES#############
+
+
+#METADATA#########
+
+#1. Get V4 object -----
+metadata.4 <- read_excel(file.path(root, "data", "Lookups", "BAMv4-results-2025-05-21.xlsx"), sheet="metadata")
+
+#PACKAGE#########
