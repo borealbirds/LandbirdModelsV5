66858e30fb4821b21198903f27bb9a116ab17022
ecknight <ellyknight@gmail.com>
2026-01-16 15:02:27 -0700

    extend to additional mosaics

--- analysis/10.Package.R
+++ analysis/10.Package.R
@@ -36,7 +36,7 @@ cc <- FALSE
 
 #3. Set nodes for local vs cluster----
 if(cc){ cores <- 32}
-if(!cc){ cores <- 6}
+if(!cc){ cores <- 4}
 
 #4. Create and register clusters----
 print("* Creating clusters *")
@@ -55,22 +55,50 @@ water <- read_sf(file.path(root, "gis", "Lakes_and_Rivers.shp"))
 
 #7. Subunit polygons----
 print("* Getting bcrs *")
-bcr.country <- read_sf(file.path(root, "gis", "Subregions_unbuffered.shp")) |> 
-  dplyr::filter(country=="can")
+bcr.country <- read_sf(file.path(root, "gis", "Subregions_unbuffered.shp")) |>
+  dplyr::filter(country=="can") |> 
+  st_union() |> 
+  st_area()
 
-#8. Mosaic polygon----
+#8. Mosaic polygons----
 print("* Mosaicing bcrs *")
-bcr.all <- st_union(bcr.country)
+
+akbox <- st_as_sfc(st_bbox(c(xmin= -3693930,
+                             ymin = 2187083,
+                             xmax = -1617275,
+                             ymax = 4562389),
+                           crs = st_crs(bcr.country)))
+
+bcr.can <- bcr.country |> 
+  dplyr::filter(country=="can") |> 
+  st_union() |> 
+  st_transform("ESRI:102001")
+
+bcr.ak <- bcr.country |> 
+  dplyr::filter(bcr %in% c("usa41423", "usa2", "usa40", "usa43", "usa5")) |> 
+  st_crop(akbox) |> 
+  st_union() |> 
+  st_transform("ESRI:102001") |> 
+  vect()
+
+bcr.48 <- bcr.country |> 
+  dplyr::filter(bcr %in% c("usa5", "usa9", "usa10", "usa11", "usa13", "usa14", "usa23", "usa28")) |> 
+  st_difference(akbox) |> 
+  st_union() |> 
+  st_transform("ESRI:102001") |> 
+  vect()
 
 #9. Load packages on clusters----
 print("* Loading packages on workers *")
 tmpcl <- clusterEvalQ(cl, library(sf))
 tmpcl <- clusterEvalQ(cl, library(tidyverse))
 tmpcl <- clusterEvalQ(cl, library(terra))
+# 
+# #10. List species for range limitation ----
+# limit <- c("CAWA", "BAWW", "BBWA", "BHVI", "BRCR", "CONW", "EVGR", "GWWA", "HAFL", "LCSP", "LEFL", "PUFI", "VATH", "VESP", "WIWR")
 
-#10. List species for range limitation ----
-limit <- c("CAWA", "BAWW", "BBWA", "BHVI", "BRCR", "CONW", "EVGR", "GWWA", "HAFL", "LCSP", "LEFL", "PUFI", "VATH", "VESP", "WIWR")
-
+#10. Data limit mask ----
+limit <- rast(file.path(root, "gis", "DataExtentMask.tif"))
 #FUNCTION###########
 
 #1. Set up the loop----
@@ -81,28 +109,30 @@ brt_package <- function(i){
   bcr.i <- loop$bcr[i]
   
   #2. Get the BCR boundary----
-  if(bcr.i!="mosaic"){
+  if(!bcr.i %in% c("Canada", "Alaska", "Lower48")){
+    
     sf.i <- bcr.country |> 
       dplyr::filter(bcr==bcr.i) |> 
-      st_transform("ESRI:102001") |> 
       vect()
+    
   } else {
-    sf.i <- bcr.all |> 
-      st_transform("ESRI:102001") |> 
-      vect()
+    
+    if(bcr.i=="Canada"){sf.i <- vect(bcr.can)}
+    if(bcr.i=="Alaska"){sf.i <- vect(bcr.ak)}
+    if(bcr.i=="Lower48"){sf.i <- vect(bcr.48)}
+
   }
   
   #3. Get list of predictions, extrapolations, sampling layers----
-  if(bcr.i!="mosaic"){
+  if(!bcr.i %in% c("Canada", "Alaska", "Lower48")){
     
     files.i <- sampled |> 
       dplyr::filter(spp==spp.i,
                     year==year.i,
                     bcr==bcr.i) |> 
+      rename(samplepath = path) |> 
       mutate(predpath = file.path(root, "output", "07_predictions", spp,
-                                  paste0(spp, "_", bcr.i, "_", year, ".tif")),
-             samplepath = file.path(root, "output", "09_sampling", spp, 
-                                    paste0(spp, "_", bcr.i, "_", year, ".tif")))
+                                  paste0(spp, "_", bcr.i, "_", year, ".tif")))
     
   } else {
     
@@ -111,7 +141,7 @@ brt_package <- function(i){
                     year==year.i,
                     bcr==bcr.i) |> 
       rename(samplepath = path) |> 
-      mutate(predpath = file.path(root, "output", "08_mosaics_can", 
+      mutate(predpath = file.path(root, "output", "08_mosaics", bcr.i, spp,
                                     paste0(spp, "_", year, ".tif")))
     
   }
@@ -141,47 +171,37 @@ brt_package <- function(i){
   q99cv <- global(cv.i, fun=function(x) quantile(x, 0.998, na.rm=TRUE))
   cv.i[[1]][values(cv.i[[1]]) > q99cv] <- q99cv[,1]
   
-  # #8. Read in the sampling distance layers----
-  # sample.i <- try(rast(files.i$samplepath) |> 
-  #                   project("ESRI:102001"))
-  # 
-  # if(inherits(sample.i, "try-error")){return(NULL)}
-  # 
-  # #9. Calculate mean sampling distance----
-  # samplemn.i <- mean(sample.i, na.rm=TRUE) |> 
-  #   resample(mean.i) |>  
-  #   crop(sf.i, mask=TRUE) |> 
-  #   mask(water, inverse=TRUE)
-  
-  #10. Stack----
-  stack.i <- c(mean.i, cv.i)
-  
-  #11. Mask outside range----
-  if(spp.i %in% limit){
-    limit.i <- try(read_sf(file.path(root, "gis", "ranges", paste0(spp.i, "_rangelimit.shp"))) |>
-                     dplyr::filter(Limit=="0.1% limit") |>
-                     st_transform(crs="ESRI:102001"))
-    
-    if(inherits(limit.i, "try-error")){return(NULL)}
-    
-    mask.i <- mask(stack.i, limit.i)
-    mask.i[is.na(mask.i)] <- 0
-    range.i <- mask.i |> 
-      crop(sf.i, mask=TRUE) |> 
-      mask(water, inverse=TRUE)
-  } else {range.i <- stack.i}
+  #8. Read in the sampling distance layers----
+  sample.i <- try(rast(files.i$samplepath) |>
+                    project("ESRI:102001"))
 
-  #12. Mask by water and NA layer ----
-  # na <- rast(file.path(root, "gis", "DataLimitationsMask.tif"))
-  # na2 <- resample(na, range.i)
-  # out.i <- range.i |> 
-  #   mask(water, inverse=TRUE) |> 
-  #   mask(na2)
-  
-  out.i <- range.i |> 
+  if(inherits(sample.i, "try-error")){return(NULL)}
+
+  #9. Calculate mean sampling distance----
+  samplemn.i <- mean(sample.i, na.rm=TRUE) |>
+    resample(mean.i) |>
+    crop(sf.i, mask=TRUE) |>
     mask(water, inverse=TRUE)
   
-  names(out.i) <- c("mean", "cv")
+  #10. Stack----
+  stack.i <- c(mean.i, cv.i, samplemn.i)
+  names(stack.i) <- c("mean", "cv", "detectiondistance")
+  
+  #11. Mask outside range----
+  limit.i <- rast(file.path(root, "gis", "ranges", paste0(spp.i, ".tif"))) |>
+    project("ESRI:102001") |> 
+    resample(stack.i)
+  
+  mask.i <- stack.i * limit.i
+  mask.i[is.na(mask.i)] <- 0
+
+  #12. Mask by water and NA layer ----
+  na <- resample(limit, mask.i)
+  
+  out.i <- mask.i |> 
+    crop(sf.i, mask=TRUE) |> 
+    mask(na) |> 
+    mask(water, inverse=TRUE)
   
   #13. Add some attributes----
   attr(out.i, "species") <- spp.i
@@ -189,16 +209,16 @@ brt_package <- function(i){
   attr(out.i, "year") <- year.i
   
   #14. Make folders as needed-----
-  if(!(file.exists(file.path(root, "output", "10_packaged_can", spp.i)))){
-    dir.create(file.path(root, "output", "10_packaged_can", spp.i))
+  if(!(file.exists(file.path(root, "output", "10_packaged", spp.i)))){
+    dir.create(file.path(root, "output", "10_packaged", spp.i))
   }
   
-  if(!(file.exists(file.path(root, "output", "10_packaged_can", spp.i, bcr.i)))){
-    dir.create(file.path(root, "output", "10_packaged_can", spp.i, bcr.i))
+  if(!(file.exists(file.path(root, "output", "10_packaged", spp.i, bcr.i)))){
+    dir.create(file.path(root, "output", "10_packaged", spp.i, bcr.i))
   }
   
   #15. Save----
-  writeRaster(out.i, filename = file.path(root, "output", "10_packaged_can", spp.i, bcr.i, paste0(spp.i, "_", bcr.i, "_", year.i, ".tif")), overwrite=TRUE)
+  writeRaster(out.i, filename = file.path(root, "output", "10_packaged", spp.i, bcr.i, paste0(spp.i, "_", bcr.i, "_", year.i, ".tif")), overwrite=TRUE)
   
 }
 
@@ -206,12 +226,12 @@ brt_package <- function(i){
 
 #1. Get the list of sampling layers----
 #remove 1985 - we're not providing those predictions
-# sampled <- data.frame(file = list.files(file.path(root, "output", "09_sampling"), pattern="*.tif", recursive = TRUE)) |> 
-#   separate(file, into=c("spf", "spp", "bcr", "year", "filetype"), remove=FALSE) |>  
-#   mutate(year = as.numeric(year),
-#          path = file.path(root, "output", "09_sampling", file)) |> 
-#   dplyr::filter(year!=1985) |> 
-#   dplyr::select(-filetype, -file, -spf)
+sampled <- data.frame(file = list.files(file.path(root, "output", "09_sampling"), pattern="*.tif", recursive = TRUE)) |>
+  separate(file, into=c("spf", "spp", "bcr", "year", "filetype"), remove=FALSE) |>
+  mutate(year = as.numeric(year),
+         path = file.path(root, "output", "09_sampling", file)) |>
+  dplyr::filter(year!=1985) |>
+  dplyr::select(-filetype, -file, -spf)
 
 predicted <-data.frame(file = list.files(file.path(root, "output", "07_predictions"), pattern="*.tif", recursive = TRUE)) |> 
   separate(file, into=c("folder", "spp", "bcr", "year", "file"), remove=FALSE) |> 
@@ -220,18 +240,17 @@ predicted <-data.frame(file = list.files(file.path(root, "output", "07_predictio
   dplyr::select(-folder, -file) |> 
   dplyr::filter(str_sub(bcr, 1, 3)=="can")
 
-mosaiced <- data.frame(file = list.files(file.path(root, "output", "08_mosaics_can"), pattern="*.tif", recursive = TRUE)) |> 
-  separate(file, into=c("spp", "year", "filetype"), remove=FALSE) |>  
+mosaiced <- data.frame(file = list.files(file.path(root, "output", "08_mosaics"), pattern="*.tif", recursive = TRUE)) |> 
+  separate(file, into=c("country", "sppfolder", "spp", "year", "filetype"), remove=FALSE) |>  
   mutate(year = as.numeric(year),
          path = file.path(root, "output", "09_sampling", file)) |> 
-  dplyr::filter(year!=1985) |> 
-  dplyr::select(-filetype, -file) |> 
-  mutate(bcr="mosaic")
-
-sampled <- rbind(predicted, mosaiced)
+  dplyr::filter(year!=1985,
+                country=="Canada") |> 
+  mutate(bcr="mosaic") |> 
+  dplyr::select(all_of(colnames(predicted)))
 
 #2. Check which have been run----
-done <- data.frame(file = list.files(file.path(root, "output", "10_packaged_can"), pattern="*.tif", recursive=TRUE))  |> 
+done <- data.frame(file = list.files(file.path(root, "output", "10_packaged"), pattern="*.tif", recursive=TRUE))  |> 
   separate(file, into=c("sppfolder", "bcrfolder", "spp", "bcr", "year", "filetype"), remove=FALSE) |> 
   mutate(year = as.numeric(year)) |> 
   dplyr::select(-filetype) |> 
@@ -242,7 +261,8 @@ done <- data.frame(file = list.files(file.path(root, "output", "10_packaged_can"
 loop <- sampled |> 
   anti_join(done) |> 
   dplyr::filter(!spp %in% c("BEKI", "SPGR", "SPSA", "CMWA"),
-                year > 1985) |> 
+                year >= 2000,
+                bcr=="Canada") |> 
   arrange(-year, spp, bcr)
 
 #PACKAGE########
